13557f0123e8a9139b8cf848f7aa0832ed816132
Nathan M. Santana <nathan.m.santana2010@gmail.com>
2026-09-25 11:49:05 -0300

    Colocação da estrutura básica do footer e do header na área de produtos junto da adição da área de administrador na área da tabela.

--- Área do administrador/tabela.js
+++ Área do administrador/tabela.js
@@ -106,8 +106,46 @@ nome.focus();
 
 function atualizarQuantidade() {
 
-
 quantidade.textContent = "Quantidade de produtos: " + totalProdutos;
 
+}
+
+const areaUsuario = document.getElementById("areaUsuario");
+
+if (localStorage.getItem("adminLogado") === "true") {
+
+    areaUsuario.innerHTML = `
+        <img src="../Área do administrador/logo2.png"
+             class="foto-admin">
+    `;
+
+}
+
+if (localStorage.getItem("adminLogado") === "true") {
+
+    areaUsuario.innerHTML = `
+        <div class="perfil-admin">
+
+            <img src="../Área do administrador/logo2.png" class="foto-admin">
+
+            <div class="menu-perfil">
+
+                <a href="../Área do administrador/adm.html">
+                    Entrar na área de administrador
+                </a>
+
+                <button id="sairPerfil">
+                    Sair do perfil
+                </button>
+
+            </div>
+
+        </div>
+    `;
+
+    document.getElementById("sairPerfil").addEventListener("click", function() {
+        localStorage.removeItem("adminLogado");
+        location.reload();
+    });
 
 }
